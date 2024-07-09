8d3a9aec1895dc066bb71ed81869f9b51d9cb368
yildizkubi <kubilayyildizz@gmail.com>
2024-07-09 19:03:44 +0200

    v.1.0.0

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Dark Mode Toggle
-document.getElementById("theme-toggle").addEventListener("click", function () {
-  document.body.classList.toggle("dark-mode");
-});
-
 // Smooth Scroll for Navigation Links
 document.querySelectorAll('header a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
@@ -11,23 +6,20 @@ document.querySelectorAll('header a[href^="#"]').forEach((anchor) => {
     const sectionId = this.getAttribute("href").substring(1);
     const section = document.getElementById(sectionId);
 
-    window.scrollTo({
-      top: section.offsetTop - 60, // 60px padding from top
-      behavior: "smooth",
-    });
+    if (section) {
+      window.scrollTo({
+        top: section.offsetTop - 60, // 60px padding from top
+        behavior: "smooth",
+      });
+    }
   });
-});
-
-// Toggle Menu for Mobile
-const menuToggle = document.querySelector(".menu-toggle");
-const nav = document.querySelector("header nav");
-
-menuToggle.addEventListener("click", () => {
-  nav.classList.toggle("active");
 });
 
 // Google Forms iframe Resize
 window.addEventListener("DOMContentLoaded", () => {
   const iframe = document.querySelector("iframe");
-  iframe.style.height = iframe.contentWindow.document.body.scrollHeight + "px";
+  if (iframe) {
+    iframe.style.height =
+      iframe.contentWindow.document.body.scrollHeight + "px";
+  }
 });
